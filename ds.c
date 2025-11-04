1ef0583842ba24ab7aede6dc9ae0dac6d6eb6c1b
pavan-n-123 <pavankumar6789000@gmail.com>
2025-11-04 16:35:48 +0530

    Create ds.c

---
+++ ds.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#define CAP 5
+int main(){
+    int a[CAP] = {20,30,40}
+    int n = 3;
+    int x =10;
+    if(n < CAP);
+    for (int i=n;i>0;i--){
+        a[i]=a[i-1];
+    }
+    a[i]=a[i-1];
+}
+a[0]=x;
+n++;
+
+a[0]=x;
+n++;
+
+else{
+    printf("array is full");
+}
+for(int i=0;i<n;i++)
+    printf("%d",a[1]);
+}
